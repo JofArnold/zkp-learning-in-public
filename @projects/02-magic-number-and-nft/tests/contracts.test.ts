91ecbd3be344cd5757847d6656398b85c1130855
Jof <github@jofarnold.com>
2021-10-17 15:24:24 +0100

    Fix test description

--- @projects/02-magic-number-and-nft/tests/contracts.test.ts
+++ @projects/02-magic-number-and-nft/tests/contracts.test.ts
@@ -21,11 +21,12 @@ describe("NFT tests", () => {
     expect(nftOwner).to.eq(signer1.address);
   });
 
-  it("should not mint NFT id and assign NFT when given correct input", async () => {
+  it("should not mint NFT when given incorrect input", async () => {
     const proof = require("../zk/proof.json");
     const NFTPrize = await ethers.getContractFactory("NFTPrize");
     const nft = await NFTPrize.deploy();
     await nft.deployed();
+    // Make the proof invalid
     proof.proof.a = [
       "0x0c6a0d84b75abf537af3686c38a9d0d73adaf3c555e7f44c4f3fa37c7b365b15",
       "0x1e08a93f36aa7228b80a4b56fa952bc47aed36beb02a9f28f3c3f0dccdc25270",
