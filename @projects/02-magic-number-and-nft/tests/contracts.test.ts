303582a79b45bd1ff1e8285bc87d944d9636a5f4
Jof <github@jofarnold.com>
2021-10-17 19:02:40 +0100

    Replicate user entering values in browser

--- @projects/02-magic-number-and-nft/tests/contracts.test.ts
+++ @projects/02-magic-number-and-nft/tests/contracts.test.ts
@@ -1,16 +1,19 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
+const { generateJSProof } = require("../src/generateJSProof");
+
 describe("NFT tests", () => {
   it("should mint and return NFT id and assign NFT when given correct input", async () => {
     const [_, signer1] = await ethers.getSigners();
-    const proof = require("../zk/proof.json");
     const NFTPrize = await ethers.getContractFactory("NFTPrize");
     const nft = await NFTPrize.deploy();
     await nft.deployed();
+    const proof = require("../zk/proof.json");
     const transaction = await nft
       .connect(signer1)
       .validateAndMintToken(proof.proof, proof.inputs); // Try minting the token
+
     const tx = await transaction.wait(); // Waiting for the token to be minted
 
     const event = tx.events[0];
@@ -21,11 +24,11 @@ describe("NFT tests", () => {
     expect(nftOwner).to.eq(signer1.address);
   });
 
-  it("should not mint NFT when given incorrect input", async () => {
-    const proof = require("../zk/proof.json");
+  it("should not mint NFT when given incorrect input - e.g. user tinkering with proof", async () => {
     const NFTPrize = await ethers.getContractFactory("NFTPrize");
     const nft = await NFTPrize.deploy();
     await nft.deployed();
+    const proof = require("../zk/proof.json");
     // Make the proof invalid
     proof.proof.a = [
       "0x0c6a0d84b75abf537af3686c38a9d0d73adaf3c555e7f44c4f3fa37c7b365b15",
@@ -34,5 +37,33 @@ describe("NFT tests", () => {
     await expect(
       nft.validateAndMintToken(proof.proof, proof.inputs)
     ).to.be.revertedWith("invalid_proof");
+  });
+
+  it("should mint and return NFT id and assign NFT when given correct input from JS", async () => {
+    const [_, signer1] = await ethers.getSigners();
+    const proof = await generateJSProof("11111");
+    const NFTPrize = await ethers.getContractFactory("NFTPrize");
+    const nft = await NFTPrize.deploy();
+    await nft.deployed();
+    const transaction = await nft
+      .connect(signer1)
+      .validateAndMintToken(proof.proof, proof.inputs); // Try minting the token
+
+    const tx = await transaction.wait(); // Waiting for the token to be minted
+
+    const event = tx.events[0];
+    const value = event.args[2];
+    const tokenId = value.toNumber();
+    expect(tokenId).to.eq(1);
+    const nftOwner = await nft.ownerOf(1);
+    expect(nftOwner).to.eq(signer1.address);
+  });
+
+  it("should fail to generate the proof when given incorrect correct input from JS", async () => {
+    try {
+      await generateJSProof("11112");
+    } catch (e) {
+      expect(true).to.eq(true); // Lazy rather than installing extra chai packages
+    }
   });
 });
